caaeb6b541558bce59cabae0cc1229940543da79
veiv <veiv@mail.ru>
2022-07-16 01:07:03 +0300

    add info methodArray

---
+++ Example010_methodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = { 11, 12, 34, 4, 18, 55, 67, 27, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index = index + 1;
+}
